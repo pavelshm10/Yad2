7272b9c131068ac6c2cce01b118796afea5873c4
pavel <pavelsh00@gmail.com>
2022-10-23 12:02:26 +0300

    structure

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+const express = require("express");
+const routes = require("./api/routes");
+const app = express();
+var router = express.Router();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  req.sendFile(path.resolve(__dirname, "build", "index.html"));
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port `, port);
+});
