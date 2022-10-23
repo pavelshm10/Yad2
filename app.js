e28ada7807ee6fc4fadc276442348feaabc50fef
pavel <pavelsh00@gmail.com>
2022-10-23 18:33:38 +0300

    in

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var router = express.Router();
 // const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -32,5 +32,5 @@ app.use(express.static(path.join(__dirname) + "/public"));
 // }
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port `, port);
+  console.log(`Server is listening on port `, process.env.PORT);
 });
