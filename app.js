27895d3ab363236fe6e7fa527e1a8f3ca771a9f2
pavel <pavelsh00@gmail.com>
2022-10-23 22:53:43 +0300

    old cors

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ const express = require("express");
 const routes = require("./api/routes");
 const app = express();
 var router = express.Router();
-// const cors = require("cors");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3000;
 const path = require("path");
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 app.use("/", routes);
 
 // if (process.env.NODE_ENV === "production") {
