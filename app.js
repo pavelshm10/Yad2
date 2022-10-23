4e738b7bffc3781a01e45afcbbc3155f8ea01c4b
pavel <pavelsh00@gmail.com>
2022-10-23 18:05:53 +0300

    $PORT

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(express.static(path.join(__dirname) + "/public"));
 
 // }
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port `, port);
 });
