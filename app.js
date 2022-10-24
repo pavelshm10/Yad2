d0fad3e4ac1a32680a0458cb031f7243e9d8544f
pavel <pavelsh00@gmail.com>
2022-10-24 08:28:07 +0300

    last

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3000;
 const path = require("path");
+
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
--- db/mysql-init.js
+++ db/mysql-init.js
@@ -3,11 +3,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const con = mysql.createConnection({
-	host: "eu-cdbr-west-03.cleardb.net",
-	user: "b260e435482cd4",
-	password: "a5e68be0",
-	database: "heroku_5370d5802671fea",
+	// host: "eu-cdbr-west-03.cleardb.net",
+	// user: "b260e435482cd4",
+	// password: "a5e68be0",
+	// database: "heroku_5370d5802671fea",
+	host: "localhost",
+	user: "root",
+	password: `pashShmil12!`,
+	port: `3306`,
+	database: "db",
 });
+
 
 mysql://b260e435482cd4:a5e68be0@eu-cdbr-west-03.cleardb.net/heroku_5370d5802671fea?reconnect=true
 con.connect(function (err) {
