37a5ee4e9368d31d50b062ce7879fd7a9bcde754
pavel <pavelsh00@gmail.com>
2022-10-24 08:31:11 +0300

    db

--- db/mysql-init.js
+++ db/mysql-init.js
@@ -3,15 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const con = mysql.createConnection({
-	// host: "eu-cdbr-west-03.cleardb.net",
-	// user: "b260e435482cd4",
-	// password: "a5e68be0",
-	// database: "heroku_5370d5802671fea",
-	host: "localhost",
-	user: "root",
-	password: `pashShmil12!`,
-	port: `3306`,
-	database: "db",
+	host: "eu-cdbr-west-03.cleardb.net",
+	user: "b260e435482cd4",
+	password: "a5e68be0",
+	database: "heroku_5370d5802671fea",
+	// host: "localhost",
+	// user: "root",
+	// password: `pashShmil12!`,
+	// port: `3306`,
+	// database: "db",
 });
 
 
