94788c85ed25f19732172ea63b7d84295ec03686
pavel <pavelsh00@gmail.com>
2022-10-25 09:24:53 +0300

    backend deploy

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+"use strict";
+const express = require("express");
+const routes = require("./api/routes");
+const app = express();
+var router = express.Router();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT;
+const path = require("path");
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use("/", routes);
+
+app.use(express.static(path.join(__dirname) + "/public"));
+
+app.listen(port, () => {
+  console.log(`Server is listening on port `, port);
+});
